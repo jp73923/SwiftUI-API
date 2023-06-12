f297d92f669fd7893b2712a1b1aac4177ec194d6
Jay Patel <pateljay.4727@gmail.com>
2023-06-12 14:54:34 +0530

    Added API added code.

---
+++ SwiftUIAPI/SwiftUIAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPIApp.swift
+//  SwiftUIAPI
+//
+//  Created by macOS on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
